9970502d0a174f31f18b8c126eb378c698f3ab0f
Arnau Gracia López <arnaugracialopez@gmail.com>
2023-03-24 01:44:07 +0100

    Targeta licitació 1a versió

--- app/src/main/java/com/licicat/components/BriefLicitation.kt
+++ app/src/main/java/com/licicat/components/BriefLicitation.kt
@@ -1,10 +1,114 @@
 package com.licicat.components
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+
+import androidx.compose.foundation.shape.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun BriefLicitation (
-
-){
+@Preview
+fun BriefLicitation (){
+    CardWithIconAndPrice(
+        title = "Ajuntament de Barcelona",
+        description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        icon = Icons.Default.Person,
+        price = "198.000€",
+        date = "23/05/2023"
+    )
 
 }
+
+
+@Composable
+fun CardWithIconAndPrice(
+    icon: ImageVector,
+    title: String,
+    description: String,
+    date: String,
+    price: String
+) {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        elevation = 8.dp
+    ) {
+        var isFavorite by remember { mutableStateOf(false) }
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(48.dp)
+                        .clip(CircleShape)
+                        .background(Color.Gray)
+                ) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = "Icono de usuario",
+                        modifier = Modifier
+                            .size(32.dp)
+                            .align(Alignment.Center),
+                        tint = Color.White
+                    )
+                }
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.h6,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                    .padding(end = 2.dp)
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "Botón de favorito"
+                    )
+                }
+            }
+            Text(
+                text = description,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(top = 12.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = date,
+                    style = MaterialTheme.typography.body2
+                )
+                Text(
+                    text = price,
+                    style = MaterialTheme.typography.body2,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
+
+
