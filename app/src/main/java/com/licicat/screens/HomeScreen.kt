4377cf68d51bf632b5786756f469debfe261a27a
Martí Paulet Lòpez <78548539+martipaulet@users.noreply.github.com>
2023-05-02 12:35:41 +0200

    bug de merge faltava parentesis

--- app/src/main/java/com/licicat/screens/HomeScreen.kt
+++ app/src/main/java/com/licicat/screens/HomeScreen.kt
@@ -124,10 +124,11 @@ fun HomeScreen(navController: NavController) {
                             title = licitacio.nom_organ,
                             description = licitacio.objecte_contracte,
                             date = licitacio.termini_presentacio_ofertes.toString(),
-                            price = licitacio.pressupost_licitacio_asString+"€",
+                            price = licitacio.pressupost_licitacio_asString + "€",
                             navController = navController, // Nuevo parámetro agregado
                             location = licitacio.lloc_execucio // ubicación de la licitación
                         )
+                    }
                 }
             }
 
