52a57e9b93543fcbdad09478afe51c33a0fb1529
Abdelrahim Chelh El Azzaoui <abdelrahim.chelh@estudiantat.upc.edu>
2023-06-02 13:57:47 +0200

    fixed bug

--- app/src/main/java/com/licicat/screens/ValoracioEntitatScreen.kt
+++ app/src/main/java/com/licicat/screens/ValoracioEntitatScreen.kt
@@ -120,9 +120,11 @@ fun ValoracioEntitatScreen(navController: NavController, intent: Intent) {
                     Log.w("app", "Error getting documents: ", exception)
                 }
         }
-        Column(){
-            Column(modifier = Modifier
-                .fillMaxWidth()) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(bottom = 56.dp)) {
+            item {
                 Box(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -141,18 +143,16 @@ fun ValoracioEntitatScreen(navController: NavController, intent: Intent) {
                 }
             }
 
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                // Nota media en tiempo real
-                backgroundColor = getBackgroundColor(averageRatingState.value)
+            item {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    // Nota media en tiempo real
+                    backgroundColor = getBackgroundColor(averageRatingState.value)
 
-
-
-                // Elementos de valoración
-                Column(modifier = Modifier.fillMaxWidth()) {
+                    // Elementos de valoración
                     ratingItems.forEach { ratingItem ->
                         Card(
                             modifier = Modifier
@@ -170,30 +170,32 @@ fun ValoracioEntitatScreen(navController: NavController, intent: Intent) {
                         }
                     }
 
-                }
+                    // Comentario extra opcional
+                    TextField(
+                        value = comment,
+                        onValueChange = { newComment -> comment = newComment },
+                        label = { Text(stringResource(R.string.comentari_valoracio)) },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp)
+                    )
 
-                // Comentario extra opcional
-                TextField(
-                    value = comment,
-                    onValueChange = { newComment -> comment = newComment },
-                    label = { Text(stringResource(R.string.comentari_valoracio)) },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 16.dp)
-                )
-
-                // Botón de enviar
-                Button(
-                    onClick = { enviarValoracion(entityName, ratingItems, comment, averageRatingState, valoracio);
-                              navController.navigate(AppScreens.HomeScreen.route) {
-                            popUpTo(AppScreens.HomeScreen.route) { inclusive = true }
-                        }},
-                    modifier = Modifier.align(Alignment.End)
-                ) {
-                    Text(text = "Enviar")
+                    // Botón de enviar
+                    Button(
+                        onClick = {
+                            enviarValoracion(entityName, ratingItems, comment, averageRatingState, valoracio)
+                            navController.navigate(AppScreens.HomeScreen.route) {
+                                popUpTo(AppScreens.HomeScreen.route) { inclusive = true }
+                            }
+                        },
+                        modifier = Modifier.align(Alignment.End)
+                    ) {
+                        Text(text = "Enviar")
+                    }
                 }
             }
         }
+
 
     }
 }
